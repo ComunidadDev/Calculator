2e9c2ea0a7d64fae40a65375e90a5e53562e293e
Elvis Edson Basilio Chambi <elvis.2e3@gmail.com>
2022-12-03 19:45:59 -0400

    disenio de los botones

--- lib/ui/ui_calculator.dart
+++ lib/ui/ui_calculator.dart
@@ -67,44 +67,94 @@ class _UICalculatorState extends State<UICalculator> {
   Widget body(BuildContext ctx){
     return Expanded(
       child: Container(
-        color: Colors.deepOrange,
+        color: Colors.black,
         child: Column(
           children: [
-            Row(children: [button(ctx, "1"), button(ctx, "2"), button(ctx, "1"), button(ctx, "2")],),
-            Row(children: [button(ctx, "1"), button(ctx, "2")],),
+            Row(
+              children: [
+                button(ctx, "C", color: Colors.deepOrange, isClear: true),
+                button(ctx, "B", color: Colors.deepOrange, isDelet: true),
+                button(ctx, "%", color: Colors.deepOrange),
+                button(ctx, "/", color: Colors.deepOrange)],
+            ),
+            Row(children: [
+              button(ctx, "7"),
+              button(ctx, "8"),
+              button(ctx, "9"),
+              button(ctx, "x", color: Colors.deepOrange)
+            ],),
+            Row(children: [
+              button(ctx, "4"),
+              button(ctx, "5"),
+              button(ctx, "6"),
+              button(ctx, "-", color: Colors.deepOrange)
+            ],),
+            Row(children: [
+              button(ctx, "1"),
+              button(ctx, "2"),
+              button(ctx, "3"),
+              button(ctx, "+", color: Colors.deepOrange)
+            ],),
+            Row(children: [
+              button(ctx, ""),
+              button(ctx, "0"),
+              button(ctx, "."),
+              button(ctx, "=", color: Colors.deepOrange, isEqual: true)
+            ],),
           ],
         ),
       ),
     );
   }
 
-  Widget button(BuildContext context, String number){
-    double margin = 10.0;
+  Widget button(
+    BuildContext context,
+    String number,
+    {
+      Color color: Colors.white,
+      bool isClear: false,
+      bool isDelet: false,
+      bool isEqual: false,
+    }
+    ){
+    double margin = 10;
+    double width_box = (MediaQuery.of(context).size.width * 0.25) - (margin * 2);
     return GestureDetector(
       child: Container(
-        height: (MediaQuery.of(context).size.width * 0.25) - (margin * 2),
-        width: (MediaQuery.of(context).size.width * 0.25) - (margin * 2),
+        height: width_box,
+        width: width_box,
         margin: EdgeInsets.all(margin),
         decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(10)
+          color: Colors.white.withOpacity(0.2),
+          borderRadius: BorderRadius.circular(width_box / 2)
         ),
         child: Center(
           child: Text(
             number,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 50
+            style: TextStyle(
+              fontWeight: FontWeight.normal,
+              fontSize: 50,
+              color: color
             ),
           )
         ),
       ),
       onTap: (){
-          if((text_result + number).length <= max_size_operation){
+          if(isClear){
+            text_result = "";
+          } else if(isDelet){
+            try{
+              text_result = text_result.substring(0, text_result.length-1);
+            }catch(error){
+
+            }
+          } else if(isEqual){
+
+          } else if((text_result + number).length <= max_size_operation){
             text_result += number;
-            setState(() {
-            });
           }
+          setState(() {
+          });
       },
     );
   }
