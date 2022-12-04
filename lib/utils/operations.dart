8a296093c3a580488446565725b19bb195297130
X1010A <rodrigo.cayola.rojas@gmail.com>
2022-12-03 20:37:10 -0400

    funcion dividir_cadena

--- lib/utils/operations.dart
+++ lib/utils/operations.dart
@@ -13,3 +13,22 @@ double multiplicar(a, b) {
 double dividir(a, b) {
   return a * b;
 }
+
+List dividir_texto(String a) {
+  List lista = [];
+
+  String cadena = "";
+  a.runes.forEach((int rune) {
+    var character = new String.fromCharCode(rune);
+    if (rune >= 42 && rune <= 47) {
+      lista.add(int.parse(cadena));
+      lista.add(character);
+      cadena = "";
+    } else {
+      cadena += character;
+    }
+  });
+  lista.add(int.parse(cadena));
+
+  return lista;
+}
